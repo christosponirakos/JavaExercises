02f1071ee498ee1a6a8d76a8deec4d16b012b2db
Christos Ponirakos <cponirakos@gmail.com>
2020-03-24 07:38:03 +0200

    update date input and exception handling

--- BootCampDates/LocalDateExercise.java
+++ BootCampDates/LocalDateExercise.java
@@ -17,40 +17,52 @@ import java.util.Scanner;
  */
 public class LocalDateExercise {
 
-    private static ArrayList<BootCamp> listOfBootCamps = null;
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         List<BootCamp> listOfBootcamps = new ArrayList();
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter details for three bootcamps");
-        
-        for (int i = 1; i<4; i++) {
-            System.out.println("Please enter details for bootcamp number "+ i);
+        System.out.println("Please enter details for three bootcamps\n");
+
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Please enter details for bootcamp number " + i);
             System.out.println("Please enter a name");
             String name = input.next();
-            System.out.println("Please enter a starting date");
-            String startingDateString = input.next();
+            try {
+                System.out.println("Please enter a starting date");
+                String startingDateString = input.next();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDate startingDate = LocalDate.parse(startingDateString, formatter);
+                System.out.println("Pleease enter a ending date");
+                String endingDateString = input.next();
+                LocalDate endingDate = LocalDate.parse(endingDateString, formatter);
+
+                BootCamp boot1 = new BootCamp(name, startingDate, endingDate);
+                listOfBootcamps.add(new BootCamp(name, startingDate, endingDate));
+                
+                            } catch (Exception e)
+                {
+                    System.out.println("The date you entered is in the wrong format or invalid");
+                }
+
+            }
+            System.out.println("Please enter a date so as to check if a bootcamp is available in that date");
+            String checkDateString = input.next();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            LocalDate startingDate = LocalDate.parse(startingDateString, formatter);
-            System.out.println("Pleease enter a ending date");
-            String endingDateString = input.next();
-            LocalDate endingDate = LocalDate.parse(endingDateString, formatter);
-            listOfBootCamps.add(new BootCamp(name, startingDate, endingDate)); 
+            LocalDate checkDate = LocalDate.parse(checkDateString, formatter);
+
+            for (BootCamp bootcamp : listOfBootcamps) {
+                LocalDate startDate = bootcamp.getStartingDate();
+                LocalDate endDate = bootcamp.getEndingDate();
+                if (checkDate.isBefore(startDate)) {
+                    System.out.println("Bootcamp " + bootcamp.getName() + " will be available in " + checkDate + ". It starts " + startDate + "and ends " + endDate);
+                    break;
+                } else {
+                    System.out.println("Sorry no bootcamp available in " + checkDate);
+                }
+            }
 
         }
-        System.out.println("Please enter a date so as to check if a bootcamp is availablein that date");
-        String checkDateString = input.next();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate checkDate = LocalDate.parse(checkDateString, formatter);
-        
-        
-        for (BootCamp bootcamp:ListOfBootCamps){
-           if(checkDate.isBefore(BootCamp.getstartingDate())){ 
-        }
-        
+
     }
-
-}
